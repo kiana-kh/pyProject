d5eb91942286b4ed20b8b858bd772c9d15ee4f74
fatemeh-sadeghi <77192176+fatemeh-sadeghi@users.noreply.github.com>
2021-02-05 10:52:21 +0330

    Update project.f.py

--- project.f.py
+++ project.f.py
@@ -6,26 +6,25 @@ time1=input('enter fist time=')
 time2=input('enter second time=')
 
 #open the file
-d['time']=pd.to_datetime['time']
-d= d.sort_values(by='time')
-filteron_time(dataframe,(time1,time2))
-d=d[(d['number']== number)] 
-y=d.groupby('amount').sum()
-print(y)
+def financial_balance():
+    d['time']=pd.to_datetime['time']
+    d= d.sort_values(by='time')
+    filteron_time(dataframe,(time1,time2))
+    d=d[(d['number']== number)] 
+    y=d.groupby('amount').sum()
+    return(y)
 
 ############################################
 
 import datetime as datetime
-
-number=input('enter a number of floor=')
-d=d[(d['number']== number)] 
-z=accounts['time'].max()
-z= datetime.fromisoformat(z)
-w=z-timedelta(days=365)
-accounts=filteron_time(accounts,(z.isoformat(),w.isoformat()))
-a=accounts['Amount'].sum()
-b=(a/12)*1.2  ######### 20%نرخ تورم    
-print(b)
-
-
+def charge_estimate():
+    number=input('enter a number of floor=')
+    d=d[(d['number']== number)] 
+    z=accounts['time'].max()
+    z= datetime.fromisoformat(z)
+    w=z-timedelta(days=365)
+    accounts=filteron_time(accounts,(z.isoformat(),w.isoformat()))
+    a=accounts['Amount'].sum()
+    b=(a/12)*1.2  ######### 20%نرخ تورم    
+    return(b)
 
