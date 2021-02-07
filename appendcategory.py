412aa82b3705c4c6b4bd9d2bc93d0b82401ffbd0
kiana.kh <kiana.kh2002@gmail.com>
2021-02-07 18:49:39 +0330

    Signed-off-by: kiana

--- appendcategory.py
+++ appendcategory.py
@@ -1,44 +1,64 @@
-
-def category_input():
+categories = ["Bill","Cleaning","Elevator","Parking","Repairment","Charge"]
+subcategories = {"Bill":["Water","Electricity","Gas","Tax"]}
+           
+def category_input(categories,subcategories):
 
     loop = True
 
     while(loop):
         try:
-            categories = ["Bill","Cleaning","Elevator","Parking","Repairment","Charge"]
-            subcategories = {"Bill":["Water","Electricity","gas"]}
-            choosencategory=[]
-            inputs=[]
+             
             print("please enter the category or its number for this transaction. supported categories are:")
-            print("   ".join("{}. {}".format(i[0],i[1]) for i in enumerate(categories,1)))
-            inputs.append(input().title())
+            print("   ".join("{}. {}".format(i[0],i[1]) for i in enumerate( categories,1)),"   0. Back")
+            input_category = input().title()
         
             #adds the input in choosencategory but checks it for being number,string and indicates out of range inputs
-            if(inputs[0].isnumeric()):
-                choosencategory.append(categories[int(inputs[0])-1])
-            elif (inputs[0] in categories):
-                choosencategory.append(inputs[0])
+            if(input_category.isnumeric()):
+                if input_category=="0":
+                    choosencategory = input_category
+                else :
+                    choosencategory = categories[int(input_category)-1]
+           
+            elif (input_category in categories):
+                choosencategory = input_category
+           
             else:
                 raise ValueError 
-
-            #subcategory process. pretty similar to category one   
-            if(choosencategory[0] in subcategories):
-
-                print("This category has subcategories as listed below. please enter the subcategory or its number:")
-                print("   ".join("{}. {}".format(i[0],i[1]) for i in enumerate(subcategories[choosencategory[0]],1)))   
-                inputs.append(input().title())
-
-                if(inputs[1].isnumeric()):
-                    choosencategory.append(subcategories[choosencategory[0]][int(inputs[1])-1])
-                elif (inputs[1] in subcategories[choosencategory]):
-                    choosencategory.append(inputs[1])
-                else:
-                    raise ValueError
-            else:
-                choosencategory[1]=float('nan')
-            loop = False
         
         except(ValueError,IndexError):
             print("wrong input. please check for your spelling or wheter your number or name is within range.")
     
     return(choosencategory)
+
+
+
+def subcategory_input(categories,subcategories,choosencategory):
+    
+    loop = True
+
+    while(loop):
+        try:
+            #subcategory process. pretty similar to category one   
+            if(choosencategory in subcategories):
+
+                print("This category has subcategories as listed below. please enter the subcategory or its number:")
+                print("   ".join("{}. {}".format(i[0],i[1]) for i in enumerate(subcategories[choosencategory],1)),"   0. Back")   
+                input_subcategory = input().title()
+
+                if(input_subcategory.isnumeric()):
+                    if input_subcategory=="0":
+                        choosensubcategory = input_subcategory
+                    else :
+                        choosensubcategory = subcategories[choosencategory][int(input_subcategory)-1]
+                elif (input_subcategory in subcategories[choosencategory]):
+                    choosensubcategory = input_subcategory
+                else:
+                    raise ValueError
+            else:
+                choosensubcategory=float('nan')
+            loop = False
+        
+        except(ValueError,IndexError):
+            print("wrong input. please check for your spelling or wheter your number or name is within range.")
+    
+    return(choosensubcategory)
--- main.py
+++ main.py
@@ -5,32 +5,65 @@ import input_apartment
 import z
 import pandas as pd
 #responsible 
+#zohreh start option from 1
+#input_apartment add back option
 print("Hello. welcome")
+categories = ["Bill","Cleaning","Elevator","Parking","Repairment","Charge"]
+subcategories = {"Bill":["Water","Electricity","Gas","Tax"]}
+d={1:['id1',2,1,125,1],2:['id2',1,1,75,1],3:['id3',4,2,125,1],4:['id4',4,2,75,1],5:['id5',7,3,125,2],
+   6:['id6',3,3,75,1],7:['id7',2,4,125,1],8:['id8',6,4,75,1],9:['id9',5,5,125,2],10:['id10',5,5,75,1]}
+
+       
 while(1):
-    inputs = input("if you want to append data please type 1 and if you want to get report type 2")
+    inputs = int(input("if you want to append data please type 1 and if you want to get report type 2 setting 3"))
     if inputs==1:
-        a=input('if you want to enter the date and time manually, enter 0 and otherwise enter 1: \n')
-        time = zohreh.time(a)
-        amount = int(input("enter the amount appended:\n"))
-        category = appendcategory.category_input()
-        responsible = input_apartment.apartment('Responsible Apartment')
-##        related= [1,2,4,6,7]input("related")
-        print("""enter the division method:
-        1 Divided according to the number of inhabitants of each unit
-        2 Divided by floor number
-        3 Division according to the area of ​​each unit
-        4 Divided by the number of parking spaces
-        5 Divide evenly""")
-        devisor= int(input())
-        amountdevided = zohreh.f(z.g(d,related),amount,devisor)
-        describtion = input("enter describtion:\n")
-        data = {"Amount" : list(amountdevided.values()),
-                "Time" : time,
-                "Category" : category[0],
-                "Subcategory" : category[1],
-                "ResponsibeUnit" : responsible,
-                "RelatedUnit" : list(amountdevided.keys()),
-                "Describtion" : describtion}
+        n=0
+        inputs={}
+        while (n>=7):
+            if n==0:  
+                inputs[n]=input('if you want to enter the date and time manually, enter 0 and otherwise enter 1: \n')
+                inputs["time"]= zohreh.time(inputs[n])
+            if n==1:
+                inputs["amount"] = int(input("enter the amount appended:\n"))
+            if n==2:
+                inputs["category"] = appendcategory.category_input(categories,subcategories)
+            if n==3:
+                inputs["subcategory"] = appendcategory.category_input(categories,subcategories,inputs["category"])
+            if n==4:   
+                inputs["responsible"] = input_apartment.apartment('Responsible Apartment')
+            if n==5:
+                related= [1,2,4,6,7]##input("related")
+            if n==6:
+                print("""enter the division method:
+                      1 Divided according to the number of inhabitants of each unit
+                      2 Divided by floor number
+                      3 Division according to the area of ​​each unit
+                      4 Divided by the number of parking spaces
+                      5 Divide evenly
+                      6 devided by default
+                      0 Back""")
+                inputs["devisor"] = int(input())
+                if (inputs["devisor"] >=1 and inputs["devisor"] <= 7):
+                    inputs["amountdevided"] = zohreh.f(z.g(d,inputs["related"]),inputs["amount"],inputs["devisor"])
+                elif (inputs["devisor"] == 0):
+                    inputs["devisor"] = "0"
+                else: 
+                    print("Wrong input please try again!")
+                    continue
+            if n==7:
+                inputs["describtion"] = input("enter describtion:\n")
+            if "0" in inputs.values():
+                n -= 1
+            else:
+                n += 1
+        data = {"Amount" : list(inputs["amountdevided"].values()),
+                "Time" : inputs["time"],
+                "Category" : inputs["category"],
+                "Subcategory" : inputs["subcategory"],
+                "ResponsibeUnit" : inputs["responsible"],
+                "RelatedUnit" : list(inputs["amountdevided"].keys()),
+                "TransactionAmount" : inputs["amount"],
+                "Describtion" : inputs["describtion"]}
         dataframe = pd.DataFrame(data)
         
         with open("data/accounts.csv", mode='a') as file:
